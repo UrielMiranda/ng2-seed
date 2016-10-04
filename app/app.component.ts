d9ffda995a500845f893240b1200bc2de8d3f1d1
Uriel Miranda <urielnovoa@gmail.com>
2016-10-03 23:49:02 -0500

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by urielmiranda on 03/10/16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Angular2 Seed Project </h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by urielmiranda on 03/10/16.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
